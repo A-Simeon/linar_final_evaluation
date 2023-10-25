cc6fa1ce27e19ff67e56ecb1c0bfc665a4638680
simz_programmer <adejohsimeon01@gmail.com>
2023-10-25 17:25:02 +0100

    Create linar_final_evaluation.py

---
+++ linar_final_evaluation.py
@@ -0,0 +1,153 @@
+
+""""1) Variable Data Types 
+a) Create a variable called "Date_Of_Birth" and assign it the year you were born (or any random 
+year) using the right datatype. Print the value of the variable. 
+b) What is the difference between an integer and a floating-point number in Python? Backup your 
+explanation with an example for each.
+
+2) Basic Operations 
+a) Write a Python program that adds two numbers together and prints the result. 
+b) Write a Python program that takes a number as input and multiplies it by 10. Print the result.
+
+3) Control Structures 
+a) Write a Python program that checks if a number is even or odd. If the number is even, print 
+"Even", otherwise print "Odd". 
+b) Write a Python program that takes a number as input and checks if it is positive, negative, or 
+zero. Print the result.
+
+4) Lists and Loops 
+a) Create a list of numbers from 1 to 10. Print each number in the list using a loop. 
+b) Write a Python program that takes a list of numbers as input and returns the sum of all the 
+numbers in the list.
+c) Create a dictionary ‘colleague_name’ storing all your colleague names. Hint: Use sequence of 
+numbers as their key. 
+
+5) Functions 
+a) Write a Python function that takes three numbers as input and return their multiplication.
+b) Write a Python function that takes a list of numbers as input and returns the average of all the 
+numbers in the list.6) Libraries and Modules 
+
+a) Install and Import the "math" module and use its "sqrt" function to calculate the square root 
+of a number. 
+b) Install and Import the "random" module and use its "randint" function to generate a random 
+number between 1 and 10.
+c) Install and Import the "pywhatkit" module and use its "whatsapp" function to send a DM to 
+your tutor with the body “Good Day Sir”
+
+7) Explain the following terms relating to Python programming Language with examples where needed
+a) Escape Sequence
+b) Keywords
+c) Datatypes
+d) Dictionary 
+e) Module
+f) Interpreter
+
+8) Give a brief history of python, who built it, what led to Python and others, state the current version 
+of python you are using.
+
+9) Mentions some tools used the career listed below, write extensively on the career you are choosing 
+after this course. Explain what the career entails and the problem skilled professionals in the career
+solve in the real-world market.
+a) Data Scientist
+b) Software Engineer
+c) Data Engineer
+d) Data Analytics
+e) Web Developer (Backend Developer)
+f) Machine Learning Engineer
+
+10) Give a feedback on this Python course, your instructor and this examination.""""
+
+
+
+
+""""
+1)a
+date_of_birth = 2023
+print(date_of_birth)
+
+b) 
+interger is data type that include 0 negative number and positive number e.g o, 1, -1, 2 100 etc
+floating point number is data type that consist of decimal e.g 1.111, 2.2, 3.33, 4.4 etc
+
+
+
+2)a
+num_1 = 2
+num_2 = 3
+result = num_1 + num_2
+
+b)
+input_value = float(input("enter a number"))
+result = input_valu + 10
+print(result)
+
+
+5)a
+dev mul_value(1, 2, 4)
+    result = 1*2*4
+    return result
+mul_value(1, 2, 4)
+
+b)dev avg_list[1]
+
+
+
+
+6)a
+import math
+value = 4
+result = math.sqrt(value)
+print(result)
+
+b)
+import random
+number = ramge(1, 11)
+result = random.randint(1, 11)
+
+
+c)
+import pywhatkit
+message = "good day sir"
+result = pywhatkit.sendwhatmsg(message)
+print(result)
+
+
+
+7
+escape sequence are characrter in programming languange that cannot be used in their
+literal form unless when used with a backslash e.g #.
+
+
+keyword are reserved word or reserved character used to perform a specific operation in 
+programming language e.g in, for, if, try, except.
+
+
+datatype it consist of value that determine a variable 
+e.g interger, list, string
+
+dictionary is a in built datatype tat is unordered which can be manipulated, remove, change
+which consist of key and value 
+
+module is a funtion of a class that act as an instance of a class 
+
+interpreter it translate high level language to machine language line by line
+
+
+9)
+a) Data Scientist = ds4 jupital lab
+b) Software Engineer django ds4
+c) Data Engineer  ds4
+d) Data Analytics ds4
+e) Web Developer (Backend Developer)= django flask beutiful soup
+f) Machine Learning Engineer  ds4
+
+8)
+python was discover by "vidomenn roseumm" while studying in the university he was task to cordinate a 
+group  project for fresher stundent while working with student he discorved he couild build his own 
+programmimg software on an existing software 
+the current python version is python 3.1
+
+10)
+my cuurrent study on python as been adventurous have been able to  learn the basics of python like 
+function, operation, loop,logic, if statement, push repository on gitup and so on with the help of my 
+instructor mr abbeb who have layed the basic foundation on python and many more
